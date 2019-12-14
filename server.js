35b6c4c82fbb3fa62fc583f820398f250dd43ea4
Marko <lukovicmarko86@gmail.com>
2019-12-14 07:13:06 +0100

    Removed initial route

--- server.js
+++ server.js
@@ -17,17 +17,19 @@ const auth = require('./routes/auth');
 const contacts = require('./routes/contacts');
 const users = require('./routes/users');
 
-app.get('/', (req, res) => {
-    res.send('Hello word')
-})
+
 app.use('/api/auth', auth);
 app.use('/api/contacts', contacts);
 app.use('/api/users', users);
 
-//serve static assets in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
